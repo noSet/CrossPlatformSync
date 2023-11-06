9405a9374aec065201a3462e0e36bfa7bd5a188a
noset <342947998@qq.com>
2023-11-06 22:34:14 +0800

    添加项目文件。

---
+++ CrossPlatformSync/Program.cs
@@ -0,0 +1,100 @@
+using H.NotifyIcon.Core;
+using Microsoft.Toolkit.Uwp.Notifications;
+using Serilog;
+using System.Drawing;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+
+namespace CrossPlatformSync;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .WriteTo.File("logs/log.txt", rollingInterval: RollingInterval.Month)
+            .CreateLogger();
+
+        try
+        {
+            using var icon = new Icon(Assembly.GetExecutingAssembly().GetManifestResourceStream($"{nameof(CrossPlatformSync)}.copy_icon.ico")!);
+            using var trayIcon = new TrayIconWithContextMenu
+            {
+                Icon = icon.Handle,
+                ToolTip = "转发验证码",
+            };
+
+            trayIcon.ContextMenu = new PopupMenu
+            {
+                Items =
+            {
+                new PopupMenuItem("退出", (_, _) =>
+                {
+                    trayIcon.Dispose();
+                    Environment.Exit(0);
+                }),
+            },
+            };
+
+            trayIcon.Create();
+
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Host.UseSerilog();
+
+            var app = builder.Build();
+
+            ToastNotificationManagerCompat.OnActivated += toastArgs => Copy(toastArgs.Argument);
+            app.MapGet("/verificationcode/{code}", (string code) =>
+            {
+                new ToastContentBuilder()
+                    .AddText("验证码")
+                    .AddText(code)
+                    .AddArgument(code)
+                    .AddButton("copy", "复制验证码", ToastActivationType.Background, code)
+                    .Show(toast => toast.ExpirationTime = DateTime.Now.AddMinutes(5));
+
+                return "ok";
+            });
+
+            app.Run();
+
+            ToastNotificationManagerCompat.Uninstall();
+        }
+        catch (Exception ex)
+        {
+            Log.Logger.Fatal(ex, "致命错误");
+            throw;
+        }
+    }
+
+    static void Copy(string text)
+    {
+        if (!Win32Helpers.OpenClipboard(IntPtr.Zero))
+        {
+            Log.Logger.Debug("打开剪贴板失败。");
+            Copy(text);
+            return;
+        }
+
+        nint ptr = Marshal.StringToHGlobalUni(text);
+
+        try
+        {
+            Win32Helpers.EmptyClipboard();
+            Win32Helpers.SetClipboardData(13, ptr);
+            Win32Helpers.CloseClipboard();
+            Log.Logger.Debug("复制到剪贴板完成：{text}。", text);
+        }
+        catch (Exception ex)
+        {
+            Log.Logger.Error(ex, "Copy Error");
+        }
+        finally
+        {
+            Marshal.FreeHGlobal(ptr);
+        }
+    }
+}
---
+++ CrossPlatformSync/Win32Helpers.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.InteropServices;
+
+namespace CrossPlatformSync;
+
+internal static partial class Win32Helpers
+{
+    [LibraryImport("User32")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static partial bool CloseClipboard();
+
+    [LibraryImport("User32")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static partial bool EmptyClipboard();
+
+    [LibraryImport("User32")]
+    public static partial IntPtr GetClipboardData(int uFormat);
+
+    [LibraryImport("User32")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static partial bool IsClipboardFormatAvailable(int format);
+
+    [LibraryImport("User32")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    public static partial bool OpenClipboard(IntPtr hWndNewOwner);
+
+    [LibraryImport("User32")]
+    public static partial IntPtr SetClipboardData(int uFormat, IntPtr hMem);
+}
